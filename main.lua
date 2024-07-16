2b20ff8f5ea1b534fed30afa8db8977d43d96c9a
CRBroughton <crbroughton@posteo.uk>
2024-07-16 20:49:31 +0100

    feat: :sparkles: single circ rendered

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
 function _init()
-   
+   particles = {}
+
+    add(particles, {
+        x = 63,
+        y = 63,
+        radius = 3,
+        colour = 11,
+        speed = 1
+   })
 end
 
 function _update60()
@@ -7,5 +15,13 @@ function _update60()
 end
 
 function _draw()
-    cls(0)
+    cls()
+    for particle in all(particles) do
+        circfill(
+            particle.x,
+            particle.x,
+            particle.radius,
+            particle.colour
+        )
+    end
 end
