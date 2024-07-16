c6695e64d1613840fff28699bb8581d6cdefee87
CRBroughton <crbroughton@posteo.uk>
2024-07-16 21:00:45 +0100

    feat: :sparkles: back and forth fire

--- main.lua
+++ main.lua
@@ -1,10 +1,15 @@
 function _init()
-   particles = {}
-
-
+    particles = {}
+    force = -3
+    forced = 1
 end
 
 function _update60()
+    force += 0.1 * forced
+    if force > 3 or force < -3 then
+        forced = forced * -1
+    end
+
     for i = 1, 20 do
         add(particles, {
             x = 40 + rnd(20),
@@ -19,6 +24,8 @@ function _update60()
     for particle in all(particles) do
         particle.y -= particle.speed
         particle.life -= 1
+        particle.radius -= 0.1
+        particle.x += force
 
         if particle.life < 5 then
             particle.colour = 9
