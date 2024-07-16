63448a635cfc4e49c9c7ba5189228fb8053b91f3
CRBroughton <crbroughton@posteo.uk>
2024-07-16 20:55:14 +0100

    feat: :sparkles: more bubbles, now persists

--- main.lua
+++ main.lua
@@ -1,20 +1,28 @@
 function _init()
    particles = {}
 
+
+end
+
+function _update60()
     for i = 1, 10 do
         add(particles, {
             x = 40 + rnd(20),
             y = 80 + rnd(20),
             radius = rnd(3),
             colour = 11,
-            speed = rnd(2)
+            life = 5,
+            speed = 1 + rnd(2)
        })
     end
-end
 
-function _update60()
     for particle in all(particles) do
        particle.y -= particle.speed
+       particle.life -= 1
+
+        if particle.life < 0 then
+            del(particles, particle)
+        end
     end
 end
 
