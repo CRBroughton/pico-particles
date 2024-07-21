ac08bb3d090f73761049819f1353a0d8233953eb
CRBroughton <crbroughton@posteo.uk>
2024-07-21 21:30:53 +0100

    start drawing lines to the screen

--- fireworks.lua
+++ fireworks.lua
@@ -2,40 +2,68 @@ fireworks = {}
 
 fireworks.new = function ()
     local self = {
-        fireworks = {}
+        rockets = {}
     }
 
-    function spawn()
-        local x = 0
+    function self.drop()
+        local x = -50
         local xSpeed = 0
-        local y = 0
+        local y = -128
         local ySpeed = 0
-        local explode = false
+        local explode = true
 
         return {
             start = function(newX, newY, newXSpeed, newYSpeed)
-                if done then
-                    x, y, xSpeed, ySpeed, done = newX, newY, newXSpeed, newYSpeed, false
+                if explode then
+                    x, y, xSpeed, ySpeed, explode = newX, newY, newXSpeed, newYSpeed, false
                     return true
                 end
             end,
 
-            update = function ()
+            update = function()
+                if (explode) return
+                y -= 1
 
+                if y < 30 then
+                    explode = true
+                end
+            end,
+
+            draw = function()
+                if (explode) return
+                line(x, y, x + xSpeed, y + ySpeed, 6)
             end
         }
     end
 
-    function self.update()
+    function startFireworks()
+        local random = rnd(5)
         for i = 1, 10 do
-            self.fireworks[i].update()
+            if self.rockets[i].start(64, 100, 0, random) then
+                return
+            end
+        end
+    end
+    function self.update()
+        startFireworks()
+        for i = 1, 10 do
+            self.rockets[i].update()
         end
     end
 
     function self.draw()
+        for i = 1, 10 do
+            self.rockets[i].draw()
 
+        end
     end
 
+
+    function spawn()
+        for i = 1, 10 do
+            self.rockets[i] = self.drop()
+        end
+    end
     spawn()
     return self
 end
